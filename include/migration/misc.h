cf9d88d3fc37aec9e5187166a4dad0a01d69ff9a
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2024-04-14 20:59:30 +0200

    migration/multifd: Add migration_has_device_state_support()
    Since device state transfer via multifd channels requires multifd
    channels with packets and is currently not compatible with multifd
    compression add an appropriate query function so device can learn
    whether it can actually make use of it.
    
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>

--- include/migration/misc.h
+++ include/migration/misc.h
@@ -114,5 +114,6 @@ void dirty_bitmap_mig_init(void);
 /* migration/multifd-device-state.c */
 bool multifd_queue_device_state(char *idstr, uint32_t instance_id,
                                 char *data, size_t len);
+bool migration_has_device_state_support(void);
 
 #endif
